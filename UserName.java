4dfb8a0bff706090cc53caf91ce4f9434839c685
Meruyert Askhat <30915@iitu.edu.kz>
2023-02-12 17:32:49 +0600

    Our second commit

--- UserName.java
+++ UserName.java
@@ -1,13 +1,17 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-public class UserName {
+public class UserNameChecker {
     public static void main(String[] args){
         System.out.println("Results of checking:");
         System.out.println(checkWithRegExp("_@BEST"));
-        System.out.println(checkWithRegExp("vovan"));
+        System.out.println(checkWithRegExp("vovano"));
         System.out.println(checkWithRegExp("vo"));
         System.out.println(checkWithRegExp("Z@OZA"));
+        System.out.println(checkWithRegExp("HELLO"));
+        System.out.println(checkWithRegExp("123"));
     }
+­29­
+    Практикум по промышленному программированию ­ 2015. Лабораторная работа №3
     public static boolean checkWithRegExp(String userNameString){
         Pattern p = Pattern.compile("^[a­z0­9_­]{3,15}$");
         Matcher m = p.matcher(userNameString);
